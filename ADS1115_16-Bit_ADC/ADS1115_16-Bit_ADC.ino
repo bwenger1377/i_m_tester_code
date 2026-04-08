0feb3635403ef2fcf9867e296eca42a454f7c3ed
Benjamin Wenger <bwenger1377@gmail.com>
2026-04-08 19:59:22 -0400

    Add functionality test logic and user prompts

--- ADS1115_16-Bit_ADC/ADS1115_16-Bit_ADC.ino
+++ ADS1115_16-Bit_ADC/ADS1115_16-Bit_ADC.ino
@@ -13,13 +13,18 @@ const float multiplier = 0.1875F; // Multiplier to convert raw 16-bit value to v
 enum State {WAITING, READING, VERDICT};
 State status = WAITING;
 
+// Voltage Setting
+enum Voltage {POS,NEG};
+Voltage level = POS;
+
 // Variables to indicate if sensor works (default is yes)
 bool is_working = true;
 bool printedFail = false;
 bool printedSucc = false;
 
 int16_t adc0;  // 16-bit ADC read variable
-float voltage = 0; // Voltage reading from ADC
+float voltage = 0; // Voltage reading from ADC (+/- 5.4 V)
+float ardVoltage = 0; // Voltage reading from Arduino ADC (0-5 V)
 
 void setup() {
   // Begin serial communication
@@ -28,30 +33,104 @@ void setup() {
   delay(3000); // 3-second delay to give the user some time to prepare
 
   // Initialize the ADS1115
-  ads.begin();
-  if (!ads.begin()) {
-    Serial.println("ADS1115 not found.");
-  }
-  Serial.println("ADS1115 initialized.");
+  adsInitialize();
 
+  // Prompt user to begin testing
   Serial.println("Press SPACE + ENTER to begin testing.");
 }
 
 void loop() {
-  // Read from the ADS1115
-  adsread();
-  
-  delay(1000); // Wait for a second before the next reading
+  // Only execute code in the loop if it is not known whether or not the sensor works
+  if (!printedFail) {
+    switch (status) {
+      case WAITING: // waiting for user input
+        if (spacePressed()) {
+          status = READING; // will execute code in second case next time loop repeats
+          if (level == POS) {
+            Serial.println("Beginning test. Turn bench power supply on and set to 5.1 V. Then connect the positive lead to the A0 pin.");
+            Serial.println("Press SPACE + ENTER to conclude test.");
+          } else {
+            Serial.println("Beginning test. First, connect the banana plug between the + and GND terminals of the bench power supply.");
+            Serial.println("Turn bench power supply on and set to 1.0 V.");
+            Serial.println("Then connect the grounded terminal to GND and wire the - terminal of the power supply to the A0 pin on the ADS1115.");
+            Serial.println("Remove hand from above sensor, then press SPACE + ENTER to conclude test.");
+          }
+        }
+        break;
+      case READING:
+        // Read from the ADS1115
+        adsRead();
+
+        // Read from the Arduino analog pin for comparison
+        ardVoltage = analogRead(A0) * (5/1023); // Multiplier converts raw value to voltage
+
+        if (spacePressed()) {
+          if (level == POS) {
+            level = NEG;
+            if (voltage < 5.05) {
+              if (ardVoltage >= 5.0) {
+                is_working = false;
+                status = VERDICT;
+              }
+            } else {
+              Serial.println("Test complete. Press SPACE + ENTER to begin next test.");
+              status = WAITING;
+            }
+          } else {
+            status = VERDICT;
+            Serial.println("Test complete.");
+            if (voltage > -1.0) {
+              if (ardVoltage <= 0.1) {
+                is_working = false;
+              }
+            }
+          }
+        }
+        break;
+      case VERDICT:
+        if (!printedSucc) {
+          if (is_working) {
+            Serial.println("ADS1115 is working.");
+            printedSucc = true;
+          } else {
+            Serial.println("ADS1115 is not working.");
+            printedFail = true;
+          }
+        }
+        break;
+    }
+  }
 }
 
-void adsread() {
-  adc0 = ads.readADC_SingleEnded(0); // Read the value from channel 0
+// Function used to read from the ADS1115
+void adsRead() {
+  // Read raw value from channel 0
+  adc0 = ads.readADC_SingleEnded(0);
 
-  Serial.print("AIN0 Raw Value: ");
-  Serial.print(adc0);
   // Convert raw value to voltage in Volts
   voltage = (adc0 * multiplier) / 1000;
   Serial.print("\tVoltage: ");
   Serial.print(voltage, 4); // Print voltage with 4 decimal places
   Serial.println("V");
+}
+
+// Function called to initialize the ADS1115
+void adsInitialize() {
+  ads.begin();
+  if (!ads.begin()) {
+    Serial.println("ADS1115 not found.");
+  }
+  Serial.println("ADS1115 initialized.");
+}
+
+// Helper function to determine if spacebar was pressed
+bool spacePressed() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    while (Serial.available()) {
+      Serial.read(); // clear buffer
+    }
+    return c == ' '; // true if the first character read was a space (this function is returning a boolean)
+  }
+  return false;
 }
